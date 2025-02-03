3f9bae8c2475e8fa3b50afeb8131ba6eb93c2adf
Justin Wheeler <jjwheel336@gmail.com>
2025-02-02 23:52:57 -0500

    Create Main.java

---
+++ COSC-1200 - Assignment 1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Student firstStudent = new Student("Justin Wheeler", "100982020", 19);
+        Student secondStudent = new Student("John Williams", "100982021", 18);
+        firstStudent.displayDetails();
+        secondStudent.displayDetails();
+    }
+}
